2c275915c6f658130c2036343863762d01f1fb81
kzish <zishumbak@gmail.com>
2019-06-06 16:49:01 +0100

    added seismic code

---
+++ Siesmic.py
@@ -0,0 +1,57 @@
+import RPi.GPIO as GPIO
+import time
+from os import system
+from time import sleep
+import paho.mqtt.client as mqtt
+
+channel           = 17 # the selected gpio data pin
+topic_to_server   = "sensor_data_to_server"# when sending data to the server
+topic_from_server = "server_data_to_seismic_1"# when recieving data from the server to seismic sensor 1
+server_url        = "192.168.138.1"
+server_port       = 1883
+
+# method for seismic sensor
+def seismic_callback(channel):
+	if GPIO.input(channel):
+		client.publish(topic_to_server, "seismic_sensor_1_detected")
+	else:
+		client.publish(topic_to_server, "seismic_sensor_1_detected")
+
+# mqtt on connect callback
+def on_connect(client, userdata, flags, rc):
+    print("Mqtt connected with result code "+str(rc))
+    client.subscribe(topic_from_server)#listen for messages from the server
+
+# mqtt onmessage recieved
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))# display the message from the server
+
+
+#seismic sensor setup
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel,GPIO.IN)
+GPIO.add_event_detect(channel,GPIO.BOTH,bouncetime=300)
+GPIO.add_event_callback(channel,seismic_callback)
+
+#mqtt client setup
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(server_url, server_port, 60)
+
+
+print("Setup complete:\n")
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()        
+
+
+
+#while True:
+#	time.sleep(1)#wait 1 second
+
+
+
+
