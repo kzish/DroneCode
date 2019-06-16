4b94e93f0f7b7261b519b818202eec09322a957f
kzish <zishumbak@gmail.com>
2019-06-16 22:55:43 +0100

    drone code snapping pic

--- Capture.py
+++ Capture.py
@@ -55,6 +55,7 @@ class DroneCamera():
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     client.subscribe("droneCommands")
+    client.publish(topic_to_server, "test data exce")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
