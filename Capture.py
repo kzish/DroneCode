65f0b15894df0090f477cb0804e327736e1e2ae9
kzish <zishumbak@gmail.com>
2019-06-18 20:24:28 +0100

    1

--- Capture.py
+++ Capture.py
@@ -1,24 +1,31 @@
 import picamera
-import time
-from os import system
-from time import sleep
 import paho.mqtt.client as mqtt
-import uuid
 
 
 
+#snap a picture from th eweb console and send the image to the server
+def snap_pic_from_web_console():
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1280,720)
+        camera.capture("newpic.jpg")
+        print("picture taken")
+        #now send the picture to the server
+        #now delete the image
 
 
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("result code is: "+str(rc))
+    print("Capture is connected result code is: "+str(rc))
     client.subscribe("droneCommands")
-    client.publish("droneCommands", "test data exce")
+    client.publish("droneCommands", "test message")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(str(msg.payload))
+    command=str(msg.payload)
+    if command=="snap_pic":
+       snap_pic_from_web_console()
     
 
     
