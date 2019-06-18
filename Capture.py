0e7e992a8302bd9dec1345b7c2bd6d332514cb0c
kzish <zishumbak@gmail.com>
2019-06-18 21:45:15 +0100

    1

--- Capture.py
+++ Capture.py
@@ -1,5 +1,8 @@
-import picamera
-import paho.mqtt.client as mqtt
+import picamera#for the pi camera
+import paho.mqtt.client as mqtt #for the mqtt client
+import requests #for uploading the image
+import os #for handling files
+
 
 
 
@@ -8,9 +11,14 @@ def snap_pic_from_web_console():
     with picamera.PiCamera() as camera:
         camera.resolution = (1280,720)
         camera.capture("newpic.jpg")
-        print("picture taken")
+        print("picture taken!")
         #now send the picture to the server
+        url = 'http://192.168.138.1:5567/Admin/upload_image_from_pi'
+        files = {'file': open('newpic.jpg', 'rb')}
+        r = requests.post(url, files=files)
         #now delete the image
+        os.remove("newpic.jpg")
+        print("picture deleted!")
 
 
 
@@ -18,7 +26,7 @@ def snap_pic_from_web_console():
 def on_connect(client, userdata, flags, rc):
     print("Capture is connected result code is: "+str(rc))
     client.subscribe("droneCommands")
-    client.publish("droneCommands", "test message")
+    client.publish("droneCommands", "test message recieved!")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
