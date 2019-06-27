8219a328796cc41ceabcbd766d8ea6952a6f280e
kzish <zishumbak@gmail.com>
2019-06-27 17:14:23 +0100

    1

--- Mavlink.py
+++ Mavlink.py
@@ -1,2 +1,19 @@
 import pymavlink
-pymavlink.__doc__
+import time
+from pymavlink import mavutil
+
+
+
+master = mavutil.mavlink_connection('udpin:127.0.0.1:14550')
+master.wait_heartbeat()
+
+master.mav.command_long_send( master.target_system,   master.target_component,
+    mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+    0, 1, 0, 0, 0, 0, 0, 0)
+
+time.sleep(5)
+
+master.mav.command_long_send( master.target_system,  master.target_component,
+    mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,
+    0, 0, 0, 0, 0, 0, 0,
+    50)
